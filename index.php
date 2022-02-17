a1a17a9de851b0747732e29a7d1756c0c170a4ee
MishairaVarona <99852510+MishairaVarona@users.noreply.github.com>
2022-02-17 10:16:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am {Mishaira Varona}!</h1>
+    <u/l>
+          <li><a href="index.php">HOME</a></li
+          <li><a href="/LT2/.html">LEARNING TASK 2</a></li
+    <u/l>
+    </body>
+</html>
+    
+    
